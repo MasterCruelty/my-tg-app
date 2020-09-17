105355c7305fdae8fd20452eab0d3e469225b718
Roberto <roberto.antoniello@studenti.unimi.it>
2020-09-17 14:15:35 +0200

    commenti;modifiche minori;

--- wiki.py
+++ wiki.py
@@ -1,17 +1,17 @@
 import wikipedia
-import os
 
+#data la lingua e la parola chiave da cercare, restituisce una frase della voce trovata
 def wiki(lang,keyword):
    wikipedia.set_lang(lang)
    result = wikipedia.summary(keyword,sentences = 1) 
    return result
-
+#data la lingua e la parola chiave da cercare, restituisce il numero massimo di frasi(limite della libreria) della voce trovata
 def wikiall(lang,keyword):
    wikipedia.set_lang(lang)
    result = wikipedia.summary(keyword,sentences = 10)
    result = result.replace("==","****")
    return result
-
+#data la lingua restituisce una frase di una pagina wikipedia casuale
 def wikirandom(lang):
     wikipedia.set_lang(lang)
     random = wikipedia.random()
