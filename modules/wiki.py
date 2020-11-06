3cd464b17c031a6f3a57d60188a1e7f0d6543b62
Roberto <roberto.antoniello@studenti.unimi.it>
2020-11-06 16:05:58 +0100

    integrata wikirandom in wikiall

--- modules/wiki.py
+++ modules/wiki.py
@@ -8,12 +8,15 @@ def wiki(lang,keyword):
 #data la lingua e la parola chiave da cercare, restituisce il numero massimo di frasi(limite della libreria) della voce trovata
 def wikiall(lang,keyword):
    wikipedia.set_lang(lang)
+   if "random" in keyword:
+       result = wikirandom(lang,10)
+       return result
    result = wikipedia.summary(keyword,sentences = 10)
    result = result.replace("==","****")
    return result
 #data la lingua restituisce una frase di una pagina wikipedia casuale
-def wikirandom(lang):
+def wikirandom(lang,sents):
     wikipedia.set_lang(lang)
     random = wikipedia.random()
-    result = wikipedia.summary(random,sentences=1)
+    result = wikipedia.summary(random,sentences=sents)
     return result
