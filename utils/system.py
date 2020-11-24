a14284b0c5b9636ccea32f75f9e6f5f5057839ac
Roberto <roberto.antoniello@studenti.unimi.it>
2020-11-24 19:21:28 +0100

    added tool for commands' execution

--- utils/system.py
+++ utils/system.py
@@ -3,6 +3,9 @@ import time
 import os
 import re
 import utils_config
+#import sys
+#sys.path.append(sys.path[0] + "/..")
+import modules.wiki
 
 """
 Funzione che preleva i dati dal file di configurazione json
@@ -10,6 +13,25 @@ Funzione che preleva i dati dal file di configurazione json
 def get_config_file(json_file):
     config = utils_config.load_config(json_file)
     return utils_config.serialize_config(config)
+
+"""
+Questa funzione prende come argomento il match e la richiesta dal main e dirotta la richiesta sul file dedicato a quel comando
+"""
+def fetch_command(chat,message_id,match,query):
+    if match == "/wiki":
+        modules.wiki.execute_wiki(chat,message_id,query)
+    if match == "/map":
+        modules.gmaps.execute_map(chat,message_id,query)
+    if match == "/km":
+        modules.gmaps.execute_km(chat,message_id,query)
+    if match == "/route":
+        modules.gmaps.execute_route(chat,message_id,query)
+    if match == "/lyrics":
+        modules.lyrics.execute_lyrics(chat,message_id,query)
+    if match == "/atm":
+        modules.atm_feature.execute_atm(chat,message_id,query)
+    if match == "/covid":
+        modules.covid.execute_covid(chat,message_id,query)
 
 """
 funzione che aiuta a parsare i comandi nel sorgente principale senza sporcare troppo in giro
