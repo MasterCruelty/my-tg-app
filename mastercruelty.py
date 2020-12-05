b1e7c3ff71738daab32bb260f914708b1a41ff2c
Roberto <roberto.antoniello@studenti.unimi.it>
2020-12-05 15:32:59 +0100

    search_messages moved into sysfunctions

--- mastercruelty.py
+++ mastercruelty.py
@@ -15,7 +15,6 @@ comandi_admin = config["lista_comandi_admin"]
 comandi_super = config["lista_comandi_super"] 
 app = Client("my_account", api_id, api_hash)
 time_range = DateTimeRange("16:40:00","17:20:00")
-endsearchmsg = False
 
 @app.on_message()
 def print_updates(client,message):
@@ -26,7 +25,6 @@ def print_updates(client,message):
     id_messaggio = message["message_id"]
     time_message = time.strftime("%H:%M:%S")
     file_id = "Nullo"
-    global endsearchmsg #this is useful to stop the search of messages
     
     if message["from_user"]["username"] is None:
         username = "Non impostato"
@@ -44,32 +42,6 @@ def print_updates(client,message):
     #alcune funzioni di sistema
     if "/getmessage" in str(message) and (isAdmin(utente) or isSuper(utente)):
         return get_message(client,message)
-    if messaggio.startswith("/searchmsg") and (isAdmin(utente) or isSuper(utente)):
-        search = parser(messaggio)
-        return search_msg(client,message,search)
-        result = ""
-        app.send_message(chat,"Cerco i messaggi...","html",reply_to_message_id=id_messaggio)
-        count = 0
-        for message in app.search_messages(chat, query = search):
-            if not endsearchmsg and "/searchmsg" not in str(message):
-                id_msg =  message.message_id
-                if str(chat).startswith("-100"):
-                    try:
-                        result += "<a href=\"https://t.me/c/"+str(chat).replace("-100","")+"/"+str(id_msg)+"\">"+ message.text[0:15]+"...</a>" + "\n"
-                        count += 1
-                        app.edit_message_text(chat,id_messaggio+1,"Cerco i messaggi...\n"+"Messaggi trovati: "+str(count))
-                    except:
-                        continue
-                else:
-                    app.send_message(chat,"Trovato","html",reply_to_message_id=id_msg)
-                    time.sleep(2)
-        app.send_message(chat,"Trovati tutti i messaggi.\n"+ result,"html",False,False,id_messaggio)
-        endsearchmsg = False
-        return
-    if messaggio.startswith("/stopmsg") and (isAdmin(utente) or isSuper(utente)):
-        return stop_msg()
-        endsearchmsg = True
-        return
     if "/poll" in messaggio and isUser(utente):
         messaggio = parser(messaggio)
         poll = messaggio.split("/")
