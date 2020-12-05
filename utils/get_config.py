d57a21887813ab0d9f7eaff14363d9c01ae2b593
Roberto <roberto.antoniello@studenti.unimi.it>
2020-12-05 16:04:32 +0100

    added two useful functins

--- utils/get_config.py
+++ utils/get_config.py
@@ -16,3 +16,15 @@ def sendMessage(client,message,result):
     chat = message["chat"]["id"]
     client.send_message(chat,result,"html",disable_web_page_preview=True,reply_to_message_id=message["message_id"])
     return
+
+"""
+Restituisce l'id della chat
+"""
+def get_chat(message):
+    return message["chat"]["id"]
+
+"""
+Restituisce l'id del messaggio
+"""
+def get_id_msg(message):
+    return message["message_id"]
