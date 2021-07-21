939599416b3096080cd64eb1e0e010326b31342f
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2021-07-21 19:30:04 +0200

    Update dbfunctions.py
    same as here https://github.com/MasterCruelty/robbot/pull/30

--- utils/dbfunctions.py
+++ utils/dbfunctions.py
@@ -84,7 +84,9 @@ questa funzione fa una select dalla tabella User e restituisce i dati di tutti g
 def list_user(client,message):
     result = "Lista utenti salvati:\n\n"
     query = User.select()
-    if(get_chat(message) != 96000757):
+    config = get_config_file("config.json")
+    id_super_admin = config["id_super_admin"].split(";")
+    if(get_chat(message) != id_super_admin[0]):
         for user in query:
             try:
                 client.get_chat_member(get_chat(message),user.id_user)
