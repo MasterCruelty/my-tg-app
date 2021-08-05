95c15cd7f08a81235f90a73ccc0a171dae509b49
Roberto <robertino.antoniello@gmail.com>
2021-08-05 17:02:20 +0200

    Fix variable name

--- modules/gmaps.py
+++ modules/gmaps.py
@@ -81,8 +81,8 @@ def directions(client,message,address1,address2):
     coord1 = coord1[::-1]
     coord2 = coord2[::-1]
     coords = ((coord1[0],coord1[1]),(coord2[0],coord2[1]))
-    client = openrouteservice.Client(key = api_geopy)
-    travel = client.directions(coords,profile='driving-car',format='json',preference = 'fastest',units='km',language="it")
+    client_geopy = openrouteservice.Client(key = api_geopy)
+    travel = client_geopy.directions(coords,profile='driving-car',format='json',preference = 'fastest',units='km',language="it")
     dis_time = travel['routes'][0]['summary']
     distanza = dis_time['distance']
     distanza = round(distanza,2)
