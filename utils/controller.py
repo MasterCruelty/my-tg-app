e98304283d54d77608334276d405ec438766a561
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2022-02-05 00:38:38 +0100

    Update controller.py
    same refactor as here https://github.com/MasterCruelty/robbot/pull/52

--- utils/controller.py
+++ utils/controller.py
@@ -5,9 +5,9 @@ import modules.gmaps
 import modules.lyrics
 import modules.atm_feature
 import modules.covid
-import utils.dbfunctions
-import utils.sysfunctions
-import utils.get_config
+import utils.dbfunctions as udb
+import utils.sysfunctions as usys
+import utils.get_config as ugc
 
 
 dictionary = {      '/wiki'       : modules.wiki.execute_wiki,
@@ -18,33 +18,33 @@ dictionary = {      '/wiki'       : modules.wiki.execute_wiki,
                     '/geoatm'     : modules.atm_feature.geodata_stop,
                     '/searchatm'  : modules.atm_feature.search_line,
                     '/covid'      : modules.covid.covid_cases,
-                    '/poll'       : utils.sysfunctions.poll_function,
-                    '/help'       : utils.sysfunctions.help}
+                    '/poll'       : usys.poll_function,
+                    '/help'       : usys.help}
 
-dictionary_admin = {'/hcount'     : utils.sysfunctions.count_messages,
+dictionary_admin = {'/hcount'     : usys.count_messages,
                     '/route'      : modules.gmaps.execute_route,
-                    '/id'         : utils.sysfunctions.id_chat,
-                    '/getid'      : utils.sysfunctions.get_id,
-                    '/getuser'    : utils.sysfunctions.get_user,
-                    '/getmessage' : utils.sysfunctions.get_message,
-                    '/playlotto'  : utils.sysfunctions.play_lotto,
-                    '/searchmsg'  : utils.sysfunctions.search_msg,
-                    '/stopmsg'    : utils.dbfunctions.stop_msg_true,
-                    '/ping'       : utils.sysfunctions.ping}
+                    '/id'         : usys.id_chat,
+                    '/getid'      : usys.get_id,
+                    '/getuser'    : usys.get_user,
+                    '/getmessage' : usys.get_message,
+                    '/playlotto'  : usys.play_lotto,
+                    '/searchmsg'  : usys.search_msg,
+                    '/stopmsg'    : udb.stop_msg_true,
+                    '/ping'       : usys.ping}
 
-dictionary_super = {'/setuser'    : utils.dbfunctions.set_user,
-                    '/deluser'    : utils.dbfunctions.del_user,
-                    '/listuser'   : utils.dbfunctions.list_user,
-                    '/alluser'    : utils.dbfunctions.all_user,
-                    '/setadmin'   : utils.dbfunctions.set_admin,
-                    '/deladmin'   : utils.dbfunctions.del_admin,
-                    '/send'       : utils.sysfunctions.send_file,
-                    '/exec'       : utils.sysfunctions.exec_file}
+dictionary_super = {'/setuser'    : udb.set_user,
+                    '/deluser'    : udb.del_user,
+                    '/listuser'   : udb.list_user,
+                    '/alluser'    : udb.all_user,
+                    '/setadmin'   : udb.set_admin,
+                    '/deladmin'   : udb.del_admin,
+                    '/send'       : usys.send_file,
+                    '/exec'       : usys.exec_file}
 """
 Questa funzione prende come argomento il match e la richiesta dal main e dirotta la richiesta sul file dedicato a quel comando
 """
 def fetch_command(match,query,client,message):
-    if match in dictionary and check_group(client,message):
+    if check_group(client,message):
         return dictionary[match](query,client,message)
 
 """
@@ -52,22 +52,20 @@ Analogamente a fetch_command ma per i comandi esclusivi degli utenti admin
 """
 def fetch_admin_command(match,query,client,message):
     #system functions
-    if match in dictionary_admin:
-        try:
-            return dictionary_admin[match](client,message,query)
-        except:
-            return dictionary_admin[match](client,message)
+    try:
+        return dictionary_admin[match](client,message,query)
+    except:
+        return dictionary_admin[match](client,message)
 
 """
 Analogamente a fetch_command ma per i comandi esclusivi del super admin
 """
 def fetch_super_command(match,query,client,message):
     #db functions and send_file
-    if match in dictionary_super:
-        try:
-            return dictionary_super[match](client,message,query)
-        except:
-            return dictionary_super[match](client,message)
+    try:
+        return dictionary_super[match](client,message,query)
+    except:
+        return dictionary_super[match](client,message)
 
 """
 controlla che robbot non sia nella stessa chat, altrimenti esegue il comando
