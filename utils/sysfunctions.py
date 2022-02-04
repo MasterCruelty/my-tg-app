62791e0ea16de46b442589aeff0ae086fa644a68
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2022-02-05 00:42:27 +0100

    Update sysfunctions.py
    same refactor as here https://github.com/MasterCruelty/robbot/pull/52
    an issue fixed in #37

--- utils/sysfunctions.py
+++ utils/sysfunctions.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
-import utils.controller
-import utils.get_config
-import utils.dbfunctions
+import utils.controller as uct
+import utils.get_config as ugc
+import utils.dbfunctions as udb
 import random
 import time
 import os
@@ -11,10 +11,10 @@ Ricerca ogni messaggio che matcha con la keyword richiesta nella chat in cui vie
 """
 @Client.on_message()
 def search_msg(client,message,search):
-    utils.dbfunctions.stop_msg_false()
+    udb.stop_msg_false()
     endsearchmsg = False
-    chat = utils.get_config.get_chat(message)
-    id_messaggio = utils.get_config.get_id_msg(message)
+    chat = ugc.get_chat(message)
+    id_messaggio = ugc.get_id_msg(message)
     result = ""
     client.send_message(chat,"Cerco i messaggi...","html",reply_to_message_id=id_messaggio)
     count = 0
@@ -30,7 +30,7 @@ def search_msg(client,message,search):
                     continue
             else:
                 client.send_message(chat,"Trovato","html",reply_to_message_id=id_msg)
-                endsearchmsg = utils.dbfunctions.isTrueStop()
+                endsearchmsg = udb.isTrueStop()
                 time.sleep(2)
     client.send_message(chat,"Trovati tutti i messaggi.\n"+ result,"html",False,False,id_messaggio)
 
@@ -39,8 +39,8 @@ Lancia un sondaggio in automatico non anonimo
 """
 @Client.on_message()
 def poll_function(query,client,message):
-    chat = utils.get_config.get_chat(message)
-    id_messaggio = utils.get_config.get_id_msg(message)
+    chat = ugc.get_chat(message)
+    id_messaggio = ugc.get_id_msg(message)
     poll = query.split("/")
     domanda = poll[0]
     opzioni = poll[1]
@@ -52,7 +52,7 @@ Preso come argomento un path intero, invia quel file su telegram(utile per backu
 """
 @Client.on_message()
 def send_file(client,message,path):
-    chat = utils.get_config.get_chat(message)
+    chat = ugc.get_chat(message)
     try:
         client.send_document(chat,document = path,caption = "__Ecco il file richiesto__",reply_to_message_id=message["message_id"])
     except:
@@ -63,7 +63,7 @@ Esegue un comando dalla shell della macchina su cui è hostato lo userbot e mand
 """
 @Client.on_message()
 def exec_file(client,message,src):
-    chat = utils.get_config.get_chat(message)
+    chat = ugc.get_chat(message)
     try:
         os.system(src + " > out.txt")
         out = open("out.txt",'r')
@@ -77,10 +77,10 @@ Restituisce il numero di messaggi complessivo nella chat in cui viene lanciato i
 """
 @Client.on_message()
 def count_messages(client,message):
-    chat = utils.get_config.get_chat(message)
+    chat = ugc.get_chat(message)
     totmsg = client.get_history_count(chat)
     result = "Totale messaggi in questa chat: " + str(totmsg)
-    return utils.get_config.sendMessage(client,message,result)
+    return ugc.sendMessage(client,message,result)
 
 """
 Restituisce sotto forma di messaggio Telegram l'id della chat corrente
@@ -88,7 +88,7 @@ Restituisce sotto forma di messaggio Telegram l'id della chat corrente
 @Client.on_message()
 def id_chat(client,message):
     chat_id = message["chat"]["id"]
-    return utils.get_config.sendMessage(client,message,chat_id)
+    return ugc.sendMessage(client,message,chat_id)
 
 """
 Restituisce l'id dell'utente a cui appartiene il messaggio risposto
@@ -96,7 +96,7 @@ Restituisce l'id dell'utente a cui appartiene il messaggio risposto
 def get_id(client,message):
     content = message["reply_to_message"]["from_user"]
     result = content["id"]
-    return utils.get_config.sendMessage(client,message,result)
+    return ugc.sendMessage(client,message,result)
 
 """
 Restituisce il json dell'utente richiesto(id utente come argomento)
@@ -104,57 +104,57 @@ Restituisce il json dell'utente richiesto(id utente come argomento)
 @Client.on_message()
 def get_user(client,message,query):
     info_user = client.get_users(query)
-    return utils.get_config.sendMessage(client,message,info_user)
+    return ugc.sendMessage(client,message,info_user)
 
 """
 Restituisce il json intero di un messaggio. Se il json supera la capacità di un messaggio Telegram, viene inviato sotto forma di file.
 """
 @Client.on_message()
 def get_message(client,message):
-    check = utils.controller.check_group(client,message)
+    check = uct.check_group(client,message)
     if(check):
-        chat = utils.get_config.get_chat(message)
-        utils.controller.save_json(message)
+        chat = ugc.get_chat(message)
+        uct.save_json(message)
         client.send_document(chat,document = "json_message.json",caption = "__Ecco il json prodotto dal messaggio__",reply_to_message_id=message["message_id"])
 
 """
 Veloce controllo se l'app è online
 """
 def ping(client,message):
-    return utils.get_config.sendMessage(client,message,"pong")
+    return ugc.sendMessage(client,message,"pong")
 
 """
 documentazione dei comandi utente direttamente su Telegram
 """
 def help(query,client,message):
-    help_file = utils.get_config.get_config_file("help.json")
+    help_file = ugc.get_config_file("help.json")
     if "wiki" in query:
         help_wiki = help_file["wiki"][0]
         help_wikiall = help_file["wiki"][1]
         help_wikirandom = help_file["wiki"][2]
         help_comune = help_file["wiki"][3]
-        return utils.get_config.sendMessage(client,message,help_wiki+"\n\n"+help_wikiall+"\n\n"+help_wikirandom+"\n\n"+help_comune)
+        return ugc.sendMessage(client,message,help_wiki+"\n\n"+help_wikiall+"\n\n"+help_wikirandom+"\n\n"+help_comune)
     if "lyrics" in query:
         help_lyrics = help_file["lyrics"]
-        return utils.get_config.sendMessage(client,message,help_lyrics)
+        return ugc.sendMessage(client,message,help_lyrics)
     if "covid" in query:
         help_covid = help_file["covid"]
-        return utils.get_config.sendMessage(client,message,help_covid)
+        return ugc.sendMessage(client,message,help_covid)
     if "poll" in query:
         help_poll = help_file["poll"]
-        return utils.get_config.sendMessage(client,message,help_poll)
+        return ugc.sendMessage(client,message,help_poll)
     if "mappe" in query:
         help_map = help_file["mappe"][0]
         help_km = help_file["mappe"][1]
         help_route = help_file["mappe"][2]
-        return utils.get_config.sendMessage(client,message,help_map+"\n\n"+help_km+"\n\n"+help_route)
+        return ugc.sendMessage(client,message,help_map+"\n\n"+help_km+"\n\n"+help_route)
     if "atm" in query:
         help_atm = help_file["atm"][0]
         help_geoatm = help_file["atm"][1]
         help_searchatm = help_file["atm"][2]
-        return utils.get_config.sendMessage(client,message,help_atm+"\n\n"+help_geoatm+"\n\n"+help_searchatm)
+        return ugc.sendMessage(client,message,help_atm+"\n\n"+help_geoatm+"\n\n"+help_searchatm)
     else:
-        return utils.get_config.sendMessage(client,message,"Cerca un comando in particolare come ad esempio:\n /help 'comando'\n__Comandi: wiki, lyrics, covid, poll, atm e mappe.__")
+        return ugc.sendMessage(client,message,"Cerca un comando in particolare come ad esempio:\n /help 'comando'\n__Comandi: wiki, lyrics, covid, poll, atm e mappe.__")
 
 """
 Restituisce 6 numeri tutti diversi tra loro tutti nel range da 1 a 90
@@ -167,4 +167,4 @@ def play_lotto(client,message):
         if n not in numbers:
             numbers.append(n)
     result = ' '.join(str(n) for n in numbers)
-    return utils.get_config.sendMessage(client,message,result)
+    return ugc.sendMessage(client,message,result)
